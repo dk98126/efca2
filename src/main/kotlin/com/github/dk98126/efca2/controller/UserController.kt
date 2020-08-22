60967841a80603279920b6a7a07e47dc9c624090
Daniil Krasnov <dk98126@yandex.ru>
2020-08-22 18:18:31 +0300

    made user repo ReactiveCrudRepository

--- src/main/kotlin/com/github/dk98126/efca2/controller/UserController.kt
+++ src/main/kotlin/com/github/dk98126/efca2/controller/UserController.kt
@@ -3,19 +3,22 @@ package com.github.dk98126.efca2.controller
 import com.github.dk98126.efca2.repo.UserRepo
 import com.github.dk98126.efca2.model.User
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.web.bind.annotation.*
 
 @RestController()
 @RequestMapping("/user")
 class UserController(private val userRepo: UserRepo) {
     @GetMapping("/all")
-    fun findAll(): Flow<User> = userRepo.findAll()
+    fun findAll(): Flow<User> = userRepo.findAll().asFlow()
 
     @GetMapping("/count")
-    suspend fun count(): Long = userRepo.count()
+    suspend fun count(): Long = userRepo.count().awaitSingle()
 
     @GetMapping("/{id}")
-    suspend fun findOne(@PathVariable id: Long): User? = userRepo.findOne(id)
+    suspend fun findOne(@PathVariable id: Long): User? = userRepo.findById(id).awaitFirstOrNull()
 
     @PostMapping
     suspend fun save(@RequestBody user: User) = userRepo.save(user)
--- src/main/kotlin/com/github/dk98126/efca2/repo/UserRepo.kt
+++ src/main/kotlin/com/github/dk98126/efca2/repo/UserRepo.kt
@@ -1,22 +1,6 @@
 package com.github.dk98126.efca2.repo
 
 import com.github.dk98126.efca2.model.User
-import kotlinx.coroutines.flow.Flow
-import org.springframework.data.r2dbc.core.*
-import org.springframework.stereotype.Repository
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
 
-@Repository
-class UserRepo(private val client: DatabaseClient) {
-    suspend fun count(): Long = client.execute("SELECT count(*) FROM users").asType<Long>().fetch().awaitOne()
-
-    fun findAll(): Flow<User> = client.select().from("users").asType<User>().fetch().flow()
-
-    suspend fun findOne(id: Long): User? =
-            client.execute("SELECT * FROM users WHERE id = :id")
-                    .bind("id", id)
-                    .asType<User>()
-                    .fetch()
-                    .awaitOneOrNull()
-
-    suspend fun save(user: User) = client.insert().into(user.javaClass).table("users").using(user).await()
-}
+interface UserRepo: ReactiveCrudRepository<User, Long>
