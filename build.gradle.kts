d30f8291c5da6e9d4893d180e6528abd02d30d32
Daniil Krasnov <dk98126@yandex.ru>
2020-08-30 23:19:34 +0300

    separated into two modules and added some examples to kotlin js code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,67 +1,18 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
-
 plugins {
-    id("org.springframework.boot") version "2.3.3.RELEASE"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
-
     val kotlinVersion = "1.4.0"
-    kotlin("jvm") version kotlinVersion
-    kotlin("plugin.allopen") version kotlinVersion
-    kotlin("plugin.noarg") version kotlinVersion
-    kotlin("plugin.spring") version kotlinVersion
-    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("jvm") version kotlinVersion apply false
+    kotlin("js") version kotlinVersion apply false
 }
 
-group = "com.github.dk98126"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    val springDocOpenApiVersion = "1.4.5"
-
-//    spring
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-    implementation("org.springdoc:springdoc-openapi-webflux-ui:$springDocOpenApiVersion")
-
-//    db
-    implementation("org.postgresql:postgresql")
-    implementation("io.r2dbc:r2dbc-postgresql")
-    implementation("org.liquibase:liquibase-core")
-
-//    kotlin
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-//    test
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+subprojects {
+    if (name == "efca2-back") {
+        apply(plugin = "org.jetbrains.kotlin.jvm")
     }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+    if (name == "efca2-front") {
+        apply(plugin = "org.jetbrains.kotlin.js")
     }
-}
 
-tasks.getByName<BootBuildImage>("bootBuildImage") {
-    imageName = "efca-back"
+    repositories {
+        mavenCentral()
+    }
 }
--- efca2-back/src/main/kotlin/com/github/dk98126/efca2/Efca2Application.kt
+++ efca2-back/src/main/kotlin/com/github/dk98126/efca2/Efca2Application.kt
@@ -2,7 +2,6 @@ package com.github.dk98126.efca2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
 class Efca2Application
